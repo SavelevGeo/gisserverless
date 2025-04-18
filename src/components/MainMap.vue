f4132b0df6c8c2e8695a3a41015496d91e624432
Georgiy Savelyev <savelevgeo.post@gmail.com>
2025-04-18 23:00:42 +0300

    fix layer order

--- src/components/MainMap.vue
+++ src/components/MainMap.vue
@@ -16,17 +16,17 @@ const vectorUrl: Ref<string> = ref("pbf/spb_2/{z}/{x}/{y}.pbf");
   >
     <ol-view :center="[3375000, 8380000]" :zoom="11" />
 
-    <ol-vector-tile-layer>
-      <ol-source-vector-tile :url="vectorUrl" :format="mvtFormat">
-      </ol-source-vector-tile>
-    </ol-vector-tile-layer>
-
+    <ol-tile-layer :opacity="0.5">
+      <ol-source-osm />
+    </ol-tile-layer>
+    
     <ol-tile-layer>
       <ol-source-xyz :url="rasterUrl" />
     </ol-tile-layer>
 
-    <ol-tile-layer :opacity="0.5">
-      <ol-source-osm />
-    </ol-tile-layer>
+    <ol-vector-tile-layer>
+      <ol-source-vector-tile :url="vectorUrl" :format="mvtFormat">
+      </ol-source-vector-tile>
+    </ol-vector-tile-layer>
   </ol-map>
 </template>
